857f91dad554ab28aff320ed27162150ccf61762
Gonzalo Seriche <geseriche@falabella.cl>
2021-06-26 15:21:09 -0400

    Created the two files and copied to the s3 bucket created

--- main.tf
+++ main.tf
@@ -7,3 +7,25 @@ resource "aws_s3_bucket" "my_bucket" {
     Environment = "Test"
   }
 }
+
+resource "local_file" "test1" {
+  content  = timestamp()
+  filename = "test1.txt"
+}
+
+resource "local_file" "test2" {
+  content  = timestamp()
+  filename = "test2.txt"
+}
+
+resource "aws_s3_bucket_object" "copy_test1" {
+  bucket = aws_s3_bucket.my_bucket.id
+  key    = local_file.test1.filename
+  source = local_file.test1.filename
+}
+
+resource "aws_s3_bucket_object" "copy_test2" {
+  bucket = aws_s3_bucket.my_bucket.id
+  key    = local_file.test2.filename
+  source = local_file.test2.filename
+}
