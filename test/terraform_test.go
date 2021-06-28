619db459cb0fc2636c5b540678f48aeb3dd43a6a
Gonzalo Seriche <geseriche@falabella.cl>
2021-06-27 21:10:58 -0400

    Updated the way to inform the test and not handling the error and avoid to pass with failures

--- test/terraform_test.go
+++ test/terraform_test.go
@@ -3,7 +3,6 @@ package test
 import (
   "github.com/gruntwork-io/terratest/modules/aws"
   "github.com/gruntwork-io/terratest/modules/terraform"
-  "log"
   "testing"
 )
 
@@ -20,28 +19,7 @@ func TestTerraformFlugel(t *testing.T) {
   expectedBucketName := "flugel-terraform-test"
 
   bucketRegion := terraform.Output(t, terraformOptions, "bucket_region")
-  bucketExist := aws.AssertS3BucketExistsE(t, bucketRegion, expectedBucketName)
-  if bucketExist != nil {
-    log.Println("The bucket doesn't exist")
-  }else {
-    log.Println("The bucket exists")
-  }
-
-  testFileOne, errOne := aws.GetS3ObjectContentsE(t, bucketRegion, expectedBucketName, "test1.txt")
-  testFileTwo, errTwo := aws.GetS3ObjectContentsE(t, bucketRegion, expectedBucketName, "test2.txt")
-
-  if errOne != nil {
-    log.Println("The File test1 doesn't exist")
-  }else {
-    log.Println(testFileOne)
-    log.Println("The File test1 exists")
-  }
-
-  if errTwo != nil {
-    log.Println("The File test2 doesn't exist")
-  }else {
-    log.Println(testFileTwo)
-    log.Println("The File test2 exists")
-  }
-
+  aws.AssertS3BucketExistsE(t, bucketRegion, expectedBucketName)
+  aws.GetS3ObjectContents(t, bucketRegion, expectedBucketName, "test1.txt")
+  aws.GetS3ObjectContentsE(t, bucketRegion, expectedBucketName, "test2.txt")
 }
