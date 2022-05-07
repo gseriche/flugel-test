55208333e832651ff255a9644e8599bd956cb5bb
Gonzalo Seriche <gonzalo@skitecorp.net>
2022-05-06 21:05:31 -0400

    fixed the bucket name id

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ resource "aws_s3_bucket" "my_bucket" {
 }
 
 resource "aws_s3_bucket_acl" "my_bucket" {
-  bucket = aws_s3_bucket.my_bucket
+  bucket = aws_s3_bucket.my_bucket.id
   acl    = "private"
 }
 
